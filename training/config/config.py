5f2bfc8b58113aa01f56631a4d55e7f00894165f
Raffael Luna <raffael.luna@gmail.com>
2021-02-15 17:50:57 -0300

    preprocessing prototype and first training

--- training/config/config.py
+++ training/config/config.py
@@ -1,5 +1,35 @@
-FEATURES_TO_DROP = [
-    "order_counts",
+TARGET = "category"
+
+ALL_FEATURES = [
     "product_id",
-    "seller_id"
+    "seller_id",
+    "query",
+    "search_page",
+    "position",
+    "title",
+    "concatenated_tags",
+    "creation_date",
+    "price",
+    "weight",
+    "express_delivery",
+    "minimum_quantity",
+    "view_counts",
+    "order_counts",
+    "category"
+]
+
+FEATURES_TO_DROP = [
+    "product_id",
+    "seller_id",
+    "creation_date",
+    "search_page",
+    "position",
+    "view_counts",
+    "order_counts"
+]
+
+FEATURES_TO_SCALE = [
+    "price",
+    "weight",
+    "minimum_quantity"
 ]
